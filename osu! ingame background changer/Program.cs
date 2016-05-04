6977d6fa8a81a6acdf2c5cce43452e4f81481e7d
HoLLy-HaCKeR <dario.db@hotmail.com>
2016-05-05 00:58:00 +0200

    Injector finished

--- osu! ingame background changer/Program.cs
+++ osu! ingame background changer/Program.cs
@@ -1,39 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using EasyHook;
 
 namespace osu_ibc_injector
 {
     class Program
     {
-        public static Process osuProcess;
+        public static Process InjectProcess;
         private const string DllName = "osu!ibc.dll";
-        private static string DllPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\" + DllName;
+        private static readonly string DllPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\" + DllName;
 
         static void Main(string[] args)
         {
+            //pretty stuff
             Console.Title = "osu!ibc";
             Console.WriteLine("osu!ibc - osu! ingame background changer");
             Console.WriteLine();
-            Console.Write("Waiting for osu!...");
+            Console.Write("Looking for osu!...");
 
             //get osu!
-            osuProcess = FindOsuProcess();
-            if (osuProcess == null) Exit(" did not find a single osu! process!", true);
-            Console.WriteLine(" found osu! (PID: " + osuProcess.Id + ")");
+            InjectProcess = FindOsuProcess();
+            Console.WriteLine(" found osu! (PID: " + InjectProcess.Id + ")");
 
             //inject dll
-            //if not present, ask user to select dll
-            if (!File.Exists(DllPath)) {
-                //TODO: ask for file
-                Exit("DLL file not found");
-            }
-            RemoteHooking.Inject(osuProcess.Id, InjectionOptions.DoNotRequireStrongName, DllPath, DllPath);
+            //if not present, I could ask user to select dll
+            if (!File.Exists(DllPath)) Exit(DllName + " not found");
+
+            //inject DLL into osu!
+            RemoteHooking.Inject(InjectProcess.Id, InjectionOptions.DoNotRequireStrongName, DllPath, DllPath);
+        }
+
+        public static Process FindOsuProcess()
+        {
+            //could handle length>1 in another way if needed
+            Process[] processes = Process.GetProcessesByName("osu!");
+            if (processes.Length <= 0) Exit(" did not find a single osu! process!", true);
+            if (processes.Length == 1) return processes[0];
+            if (processes.Length >= 2) Exit(" multiple osu! processes found!", true);
+            return null;    //should not occur
         }
 
         public static void Exit(string message = "Press any key to exit...", bool bad = false)
@@ -45,16 +50,6 @@ namespace osu_ibc_injector
             if (bad) Console.BackgroundColor = oldColor;
             Console.ReadKey();
             Environment.Exit(bad ? -1 : 0);
-        }
-
-        public static Process FindOsuProcess()
-        {
-            Process[] processes = Process.GetProcessesByName("osu!");
-            if (processes.Length == 0) return null;
-            if (processes.Length == 1) return processes[0];
-
-            //add handler for length >1?
-            return null;
         }
     }
 }
